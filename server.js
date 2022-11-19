1474e61ecbcb8f6f4316ded0b2b027f6c9c39f91
unknown <vuvantinh121123@gmail.com>
2022-11-19 08:23:50 +0700

    hello

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+
+const app = express();
+const path = require("path");
+
+const basePath = '';
+const port = process.env.port || 4000;
+app.use(cors());
+app.use(express.json());
+
+//cấu hình chạy reactjs trên node server
+app.use(basePath + "/", express.static(path.resolve(__dirname + "/build")));
+
+app.get("*", (request, response) => {
+  response.sendFile(path.resolve(__dirname + "build/index.html"));
+});
+//
+
+
+app.listen(port, () => {
+    console.log('server is running')
+});
